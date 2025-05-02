7476e4397948ca78747fd53ec90b4123a57b69bf
Amanv6780 <amanv6780@gmail.com>
2025-05-02 14:20:11 +0530

    regular update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Admin from "./components/Admin";
+import User from "./components/User";
+import PrivateRoute from "./components/PrivateRoute";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/admin" element={<PrivateRoute role='admin'><Admin /></PrivateRoute>} />
+        <Route path="/user" element={<PrivateRoute role='user'><User /></PrivateRoute>} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
